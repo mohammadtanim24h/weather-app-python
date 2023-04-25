6478b2cae2feb873b07caecb89683ab01675f4e0
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2023-04-25 11:56:00 +0600

    added request for weather using city entered by user

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Initialized Project")
+import requests as req
+
+print("Welcome to Weather App. Created by Tanim")
+
+# Taking input from the user
+city = input("Enter the name of the city: ")
+url = f"https://api.weatherapi.com/v1/current.json?key=0039ba140f3348fbb3f44716232504&q={city}"
+
+# Making the request using the user input
+r = req.get(url)
+weather = r.json()
+print(weather["current"])
