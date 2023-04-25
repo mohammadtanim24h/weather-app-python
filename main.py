e9bcd39bb3d881f2ce2784bef1c521f789e633aa
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2023-04-25 12:47:21 +0600

    added the feature to pronounce the weather results

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests as req
+import win32com.client as wincom
 
 print("Welcome to Weather App. Created by Tanim")
+
+speak = wincom.Dispatch("SAPI.SpVoice")
 
 # Taking input from the user
 city = input("Enter the name of the city: ")
@@ -10,7 +13,7 @@ url = f"https://api.weatherapi.com/v1/current.json?key=0039ba140f3348fbb3f447162
 r = req.get(url)
 weather = r.json()
 try:
-    print(weather["current"])
+    speak.Speak(f"The current weather in {city} is {weather['current']['condition']['text']}. The temperature is {weather['current']['temp_c']} degree celsius. It was last updated on {weather['current']['last_updated']}.")
 
 except:
     if weather["error"]["code"] == 1006:
